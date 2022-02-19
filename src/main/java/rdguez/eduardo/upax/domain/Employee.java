5c3e81736e367c18d9af949dd7db850616b5f4ce
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2022-02-19 17:42:10 -0600

    Add employee domain

---
+++ src/main/java/rdguez/eduardo/upax/domain/Employee.java
@@ -0,0 +1,39 @@
+package rdguez.eduardo.upax.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "EMPLOYEES")
+public class Employee {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(name = "ID")
+  private Long id;
+
+  @Column(name = "NAME")
+  private String name;
+
+  @Column(name = "LAST_NAME")
+  private String lastName;
+
+  @Column(name = "BIRTHDATE")
+  @JsonFormat(pattern = "yyyy-MM-dd")
+  private Date birthdate;
+
+  @ManyToOne
+  @JoinColumn(name = "GENDER_ID")
+  private Gender gender;
+
+  @ManyToOne
+  @JoinColumn(name = "JOB_ID")
+  private Job job;
+
+}

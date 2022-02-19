3a2d52402bdad01528e37be08e8f9803718caac3
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2022-02-19 17:41:56 -0600

    Add gender domain

---
+++ src/main/java/rdguez/eduardo/upax/domain/Gender.java
@@ -0,0 +1,22 @@
+package rdguez.eduardo.upax.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "GENDERS")
+public class Gender {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(name = "ID")
+  private Long id;
+
+  @Column(name = "NAME")
+  private String name;
+
+}

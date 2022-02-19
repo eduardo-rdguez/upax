35f84fc89d9d10a6dbbecffbb92229dc6f9e82f0
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2022-02-19 17:46:22 -0600

    Add employee rest controller

---
+++ src/main/java/rdguez/eduardo/upax/controller/EmployeeController.java
@@ -0,0 +1,42 @@
+package rdguez.eduardo.upax.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import rdguez.eduardo.upax.domain.Employee;
+import rdguez.eduardo.upax.model.EmployeeRequest;
+import rdguez.eduardo.upax.model.EmployeeResponse;
+import rdguez.eduardo.upax.model.EmployeeWorkedHoursRequest;
+import rdguez.eduardo.upax.service.EmployeeService;
+import rdguez.eduardo.upax.service.EmployeeWorkedHoursService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/employee")
+public class EmployeeController {
+
+  @Autowired
+  EmployeeService employeeService;
+
+  @Autowired
+  EmployeeWorkedHoursService employeeWorkedHoursService;
+
+  @ResponseBody
+  @PostMapping("/add")
+  public EmployeeResponse addEmployee(@RequestBody EmployeeRequest employeeRequest) {
+    return employeeService.addEmployee(employeeRequest);
+  }
+
+  @ResponseBody
+  @PostMapping("/worked_hours")
+  public EmployeeResponse addEmployeeWorkedHours(@RequestBody EmployeeWorkedHoursRequest employeeRequest) {
+    return employeeWorkedHoursService.addEmployeeWorkedHours(employeeRequest);
+  }
+
+  @ResponseBody
+  @PostMapping("/job")
+  public List<Employee> findEmployeesByJob(@RequestBody EmployeeRequest employeeRequest) {
+    return employeeService.findEmployeesByJob(employeeRequest);
+  }
+
+}

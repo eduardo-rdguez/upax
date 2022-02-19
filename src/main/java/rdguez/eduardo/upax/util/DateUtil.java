fb52b057d86e4781e04ce645652f9ccc38e00687
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2022-02-19 17:43:22 -0600

    Add date util

---
+++ src/main/java/rdguez/eduardo/upax/util/DateUtil.java
@@ -0,0 +1,19 @@
+package rdguez.eduardo.upax.util;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+
+public class DateUtil {
+
+  static public LocalDate currentLocalDate() {
+    return LocalDate.now();
+  }
+
+  static public LocalDate toLocalDate(Date date) {
+    return date.toInstant()
+      .atZone(ZoneId.systemDefault())
+      .toLocalDate();
+  }
+
+}

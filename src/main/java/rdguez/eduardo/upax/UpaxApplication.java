9998b26cbf604ae9627757c93b1e136af148accf
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2022-02-19 11:29:20 -0600

    Initialize a new spring boot project

---
+++ src/main/java/rdguez/eduardo/upax/UpaxApplication.java
@@ -0,0 +1,13 @@
+package rdguez.eduardo.upax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpaxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rdguez/eduardo/upax/UpaxApplicationTests.java
@@ -0,0 +1,13 @@
+package rdguez.eduardo.upax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

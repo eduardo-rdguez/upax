b745fee772fb18f0d1d02c1675f36d7c35657f09
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2022-02-19 17:42:21 -0600

    Add employee worked hours domain

---
+++ src/main/java/rdguez/eduardo/upax/domain/EmployeeWorkedHours.java
@@ -0,0 +1,30 @@
+package rdguez.eduardo.upax.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "EMPLOYEE_WORKED_HOURS")
+public class EmployeeWorkedHours {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(name = "ID")
+  private Long id;
+
+  @Column(name = "WORKED_HOURS")
+  private Integer workedHours;
+
+  @Column(name = "WORKED_DATE")
+  private Date workedDate;
+
+  @ManyToOne
+  @JoinColumn(name = "EMPLOYEE_ID")
+  private Employee employee;
+
+}

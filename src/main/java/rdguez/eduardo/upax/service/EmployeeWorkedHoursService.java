e6fe56aacb140f7f3d654a894d86d42b1b5f7c14
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2022-02-19 18:22:19 -0600

    Add methods to get total worked hours by employee and dates to service

--- src/main/java/rdguez/eduardo/upax/service/EmployeeWorkedHoursService.java
+++ src/main/java/rdguez/eduardo/upax/service/EmployeeWorkedHoursService.java
@@ -1,10 +1,18 @@
 package rdguez.eduardo.upax.service;
 
+import rdguez.eduardo.upax.domain.EmployeeWorkedHours;
 import rdguez.eduardo.upax.model.EmployeeResponse;
 import rdguez.eduardo.upax.model.EmployeeWorkedHoursRequest;
+import rdguez.eduardo.upax.model.EmployeeWorkedHoursResponse;
+
+import java.util.List;
 
 public interface EmployeeWorkedHoursService {
 
   EmployeeResponse addEmployeeWorkedHours(EmployeeWorkedHoursRequest employeeWorkedHours);
+
+  List<EmployeeWorkedHours> findWorkedHoursByDates(EmployeeWorkedHoursRequest employeeWorkedHoursRequest);
+
+  EmployeeWorkedHoursResponse findTotalWorkedHoursByDates(EmployeeWorkedHoursRequest employeeWorkedHoursRequest);
 
 }
--- src/main/java/rdguez/eduardo/upax/service/impl/EmployeeWorkedHoursServiceImpl.java
+++ src/main/java/rdguez/eduardo/upax/service/impl/EmployeeWorkedHoursServiceImpl.java
@@ -9,11 +9,13 @@ import rdguez.eduardo.upax.domain.EmployeeWorkedHours;
 import rdguez.eduardo.upax.dto.EmployeeWorkedHoursDto;
 import rdguez.eduardo.upax.model.EmployeeResponse;
 import rdguez.eduardo.upax.model.EmployeeWorkedHoursRequest;
+import rdguez.eduardo.upax.model.EmployeeWorkedHoursResponse;
 import rdguez.eduardo.upax.repository.EmployeeWorkedHoursRepository;
 import rdguez.eduardo.upax.service.EmployeeService;
 import rdguez.eduardo.upax.service.EmployeeWorkedHoursService;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -69,5 +71,42 @@ public class EmployeeWorkedHoursServiceImpl implements EmployeeWorkedHoursServic
     EmployeeWorkedHours employeeWorkedHours = employeeWorkedHoursRepository.save(newEmployeeWorkedHours);
 
     return EmployeeWorkedHoursDto.toResponse(employeeWorkedHours);
+  }
+
+  @Override
+  public List<EmployeeWorkedHours> findWorkedHoursByDates(
+    EmployeeWorkedHoursRequest employeeWorkedHoursRequest
+  ) {
+    Long employeeId = employeeWorkedHoursRequest.getEmployeeId();
+    Date startDate = employeeWorkedHoursRequest.getStartDate();
+    Date endDate = employeeWorkedHoursRequest.getEndDate();
+
+    return employeeWorkedHoursRepository.findAllByEmployee_IdAndWorkedDateBetween(
+      employeeId,
+      startDate,
+      endDate
+    );
+  }
+
+  @Override
+  public EmployeeWorkedHoursResponse findTotalWorkedHoursByDates(
+    EmployeeWorkedHoursRequest employeeWorkedHoursRequest
+  ) {
+    List<EmployeeWorkedHours> employeeWorkedHoursList = findWorkedHoursByDates(
+      employeeWorkedHoursRequest
+    );
+    Integer totalWorkedHours = sumEmployeeWorkedHoursBy(employeeWorkedHoursList);
+
+    return EmployeeWorkedHoursResponse.builder()
+      .total_worked_hours(totalWorkedHours)
+      .success(!employeeWorkedHoursList.isEmpty())
+      .build();
+  }
+
+  private Integer sumEmployeeWorkedHoursBy(List<EmployeeWorkedHours> employeeWorkedHoursList) {
+    return employeeWorkedHoursList
+      .stream()
+      .map(EmployeeWorkedHours::getWorkedHours)
+      .reduce(0, Integer::sum);
   }
 }

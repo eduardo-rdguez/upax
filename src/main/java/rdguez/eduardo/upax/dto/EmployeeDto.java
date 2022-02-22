64be1d258e0b7572422d62b0c5a4f8cc8097bdcc
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2022-02-21 22:08:54 -0600

    Refactor employee dto

--- src/main/java/rdguez/eduardo/upax/dto/EmployeeDto.java
+++ src/main/java/rdguez/eduardo/upax/dto/EmployeeDto.java
@@ -6,8 +6,6 @@ import rdguez.eduardo.upax.domain.Job;
 import rdguez.eduardo.upax.model.EmployeeRequest;
 import rdguez.eduardo.upax.model.EmployeeResponse;
 
-import java.util.Date;
-
 public class EmployeeDto {
 
   public static EmployeeResponse toResponse(Employee employee) {
@@ -18,14 +16,10 @@ public class EmployeeDto {
   }
 
   public static Employee toEntity(EmployeeRequest employeeRequest, Gender gender, Job job) {
-    String name = employeeRequest.getName();
-    String lastName = employeeRequest.getLastName();
-    Date birthdate = employeeRequest.getBirthdate();
-
     Employee employee = new Employee();
-    employee.setName(name);
-    employee.setLastName(lastName);
-    employee.setBirthdate(birthdate);
+    employee.setName(employeeRequest.getName());
+    employee.setLastName(employeeRequest.getLastName());
+    employee.setBirthdate(employeeRequest.getBirthdate());
     employee.setGender(gender);
     employee.setJob(job);
 

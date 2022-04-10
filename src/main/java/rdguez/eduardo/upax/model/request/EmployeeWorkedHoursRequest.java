57c741a9d6f1ce88027f57e92fb51270b7df10a9
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2022-04-09 23:33:58 -0500

    Add java bean validations to employee worked hours request

--- src/main/java/rdguez/eduardo/upax/model/request/EmployeeWorkedHoursRequest.java
+++ src/main/java/rdguez/eduardo/upax/model/request/EmployeeWorkedHoursRequest.java
@@ -5,7 +5,11 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import rdguez.eduardo.upax.model.request.groups.AddWorkedHours;
+import rdguez.eduardo.upax.model.request.groups.FindWorkedHours;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotNull;
 import java.util.Date;
 
 @Data
@@ -14,21 +18,27 @@ import java.util.Date;
 public class EmployeeWorkedHoursRequest {
 
   @JsonProperty("employee_id")
+  @NotNull
   private Long employeeId;
 
   @JsonProperty("worked_hours")
+  @NotNull(groups = AddWorkedHours.class)
+  @Max(value = 20, groups = AddWorkedHours.class)
   private Integer workedHours;
 
   @JsonProperty("worked_date")
   @JsonFormat(pattern = "yyyy-MM-dd")
+  @NotNull(groups = AddWorkedHours.class)
   private Date workedDate;
 
   @JsonProperty("start_date")
   @JsonFormat(pattern = "yyyy-MM-dd")
+  @NotNull(groups = FindWorkedHours.class)
   private Date startDate;
 
   @JsonProperty("end_date")
   @JsonFormat(pattern = "yyyy-MM-dd")
+  @NotNull(groups = FindWorkedHours.class)
   private Date endDate;
 
 }

17407626b70adadaa763626de1d1b5ec949711e4
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2022-04-10 12:44:21 -0500

    Update the service test of employee worked hours

--- src/test/java/rdguez/eduardo/upax/service/impl/EmployeeWorkedHoursServiceImplTest.java
+++ src/test/java/rdguez/eduardo/upax/service/impl/EmployeeWorkedHoursServiceImplTest.java
@@ -14,8 +14,6 @@ import rdguez.eduardo.upax.repository.EmployeeWorkedHoursRepository;
 import rdguez.eduardo.upax.service.EmployeeService;
 import rdguez.eduardo.upax.util.DateUtil;
 
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -31,30 +29,15 @@ class EmployeeWorkedHoursServiceImplTest {
   @Mock
   EmployeeWorkedHoursRepository employeeWorkedHoursRepository;
 
-  @Mock
-  List<EmployeeWorkedHours> employeeWorkedHoursMockList = new ArrayList<>();
-
   @Test
   void addEmployeeWorkedHours() {
-    EmployeeWorkedHoursRequest employeeWorkedHoursRequest = new EmployeeWorkedHoursRequest();
-    employeeWorkedHoursRequest.setWorkedDate(new Date());
-
     Mockito
-      .when(employeeService.findEmployeeById(Mockito.anyLong()))
-      .thenReturn(Optional.of(new Employee()));
-
-    EmployeeStatusResponse employeeStatusResponse = employeeWorkedHoursService.addEmployeeWorkedHours(
-      employeeWorkedHoursRequest
-    );
-
-    assert !employeeStatusResponse.getSuccess();
-    assert employeeStatusResponse.getId() == null;
-  }
-
-  @Test
-  void addEmployeeWorkedHours2() {
-    EmployeeWorkedHoursRequest employeeWorkedHoursRequest = new EmployeeWorkedHoursRequest();
-    employeeWorkedHoursRequest.setWorkedDate(DateUtil.stringToDate("2022-02-18"));
+      .when(
+        employeeWorkedHoursRepository.findOneByEmployee_IdAndWorkedDate(
+          Mockito.anyLong(), Mockito.any()
+        )
+      )
+      .thenReturn(Optional.empty());
 
     Mockito
       .when(employeeService.findEmployeeById(Mockito.anyLong()))
@@ -62,18 +45,18 @@ class EmployeeWorkedHoursServiceImplTest {
 
     Mockito
       .when(
-        employeeWorkedHoursRepository.findOneByEmployee_IdAndWorkedDate(
-          Mockito.anyLong(), Mockito.any()
+        employeeWorkedHoursRepository.save(
+          Mockito.any(EmployeeWorkedHours.class)
         )
       )
-      .thenReturn(Optional.of(new EmployeeWorkedHours()));
+      .thenReturn(this.mockEmployeeWorkedHours());
 
     EmployeeStatusResponse employeeStatusResponse = employeeWorkedHoursService.addEmployeeWorkedHours(
-      employeeWorkedHoursRequest
+      this.mockEmployeeWorkedHoursRequest()
     );
 
-    assert !employeeStatusResponse.getSuccess();
-    assert employeeStatusResponse.getId() == null;
+    assert employeeStatusResponse.getSuccess();
+    assert employeeStatusResponse.getId() == 1L;
   }
 
   @Test
@@ -86,31 +69,59 @@ class EmployeeWorkedHoursServiceImplTest {
           Mockito.any()
         )
       )
-      .thenReturn(employeeWorkedHoursMockList);
+      .thenReturn(this.mockEmployeeWorkedHoursList());
 
     List<EmployeeWorkedHours> employeeWorkedHoursList =
       employeeWorkedHoursService.findWorkedHoursByDates(
-        new EmployeeWorkedHoursRequest()
+        this.mockEmployeeWorkedHoursRequest()
       );
 
-    assert employeeWorkedHoursList.isEmpty();
+    assert !employeeWorkedHoursList.isEmpty();
+    assert employeeWorkedHoursList.size() == 1;
+    assert employeeWorkedHoursList.get(0).getId() == 1L;
   }
 
   @Test
   void findTotalWorkedHoursByDates() {
     Mockito
       .when(
-        employeeWorkedHoursService.findWorkedHoursByDates(
-          Mockito.mock(EmployeeWorkedHoursRequest.class)
+        employeeWorkedHoursRepository.findAllByEmployee_IdAndWorkedDateBetween(
+          Mockito.anyLong(),
+          Mockito.any(),
+          Mockito.any()
         )
       )
-      .thenReturn(employeeWorkedHoursMockList);
+      .thenReturn(this.mockEmployeeWorkedHoursList());
 
     EmployeeWorkedHoursResponse employeeWorkedHoursResponse =
       employeeWorkedHoursService.findTotalWorkedHoursByDates(
-        new EmployeeWorkedHoursRequest()
+        this.mockEmployeeWorkedHoursRequest()
       );
 
-    assert !employeeWorkedHoursResponse.getSuccess();
+    assert employeeWorkedHoursResponse.getSuccess();
+    assert employeeWorkedHoursResponse.getTotalWorkedHours() == 40;
+  }
+
+  private EmployeeWorkedHours mockEmployeeWorkedHours() {
+    EmployeeWorkedHours employeeWorkedHours = new EmployeeWorkedHours();
+    employeeWorkedHours.setId(1L);
+    employeeWorkedHours.setWorkedHours(40);
+
+    return employeeWorkedHours;
+  }
+
+  private EmployeeWorkedHoursRequest mockEmployeeWorkedHoursRequest() {
+    EmployeeWorkedHoursRequest employeeWorkedHoursRequest = new EmployeeWorkedHoursRequest();
+    employeeWorkedHoursRequest.setEmployeeId(1L);
+    employeeWorkedHoursRequest.setWorkedHours(20);
+    employeeWorkedHoursRequest.setWorkedDate(DateUtil.stringToDate("2022-02-18"));
+
+    return employeeWorkedHoursRequest;
+  }
+
+  private List<EmployeeWorkedHours> mockEmployeeWorkedHoursList() {
+    return List.of(
+      this.mockEmployeeWorkedHours()
+    );
   }
 }
